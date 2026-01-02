c4474a2d0a1abdbc1fbe8201b87d9c4488c7c7ee
Nelson-Ndirangu <nelsonmaina808@gmail.com>
2026-01-02 10:54:11 +0300

    Feat: Rate Limiting& Cors Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 // Backend server entry point
 const express = require('express');
 const dotenv = require('dotenv');
+const http = require('http');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+
 dotenv.config();
+
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoute');
 const landlordRoutes = require('./routes/landlordRoute');
@@ -10,24 +15,44 @@ const tenantRoutes = require('./routes/tenantRoute');
 const userRoutes = require('./routes/userRoute');
 const mpesaRoutes = require('./routes/mpesaRoute');
 const notificationRoutes = require('./routes/notificationRoute');
+
 require('./cron/reminder');
+
 const app = express();
 
+// Rate Limiter
+const globalLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per window
+    message: {
+        status: 429,
+        error: 'Too many requests, please try again later.'
+    },
+    standardHeaders: true,
+    legacyHeaders: false
+});
 
+// Apply rate limiter to all requests
+app.use(globalLimiter);
 
-// Middleware to parse JSON requests
+// Middleware
 app.use(express.json());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(express.urlencoded({ extended: true }));
 
-// Connect to the database
+
+// Connect to Database
 connectDB();
 
-// Define a simple route
+// Endpoints
 app.get('/', (req, res) => {
     res.send('Welcome to the Tenant Rent Management API');
 });
 
-
-// API Endpoints
 app.use('/api/auth', authRoutes);
 app.use('/api/admin/landlord', landlordRoutes);
 app.use('/api/admin/property', propertyRoutes);
@@ -36,8 +61,16 @@ app.use('/api/admin/user', userRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/mpesa', mpesaRoutes);
 
-// Start the server 
 const PORT = process.env.PORT || 6000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+// HTTP server
+const server = http.createServer(app);
+
+// Server timeout settings
+server.timeout = 30 * 1000; // 30 seconds
+server.keepAliveTimeout = 65 * 1000; // keep-alive timeout
+server.headersTimeout = 70 * 1000; // headers timeout
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
